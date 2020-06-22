7f6adfcc84f7f42a5789af686ca011897e4afe5a
Woojin Kim <me@woojinkim.org>
2020-06-22 13:25:41 +0000

    struct 플러그인 업데이트.

--- lib/plugins/struct/conf/metadata.php
+++ lib/plugins/struct/conf/metadata.php
@@ -1,15 +1,3 @@
 <?php
 
-/**
-
- * DokuWiki Plugin struct (configuration metadata)
-
- *
-
- * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
-
- * @author  Iain Hallam <iain@nineworlds.net>
-
- */
-
-$meta['show_not_found'] = array('onoff');
+$meta['bottomoutput'] = ['onoff'];
--- lib/plugins/struct/lang/en/settings.php
+++ lib/plugins/struct/lang/en/settings.php
@@ -1,17 +1,2 @@
-
-
 <?php
-
-/**
-
- * DokuWiki Plugin struct (configuration labels)
-
- *
-
- * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
-
- * @author  Iain Hallam <iain@nineworlds.net>
-
- */
-
-$lang['show_not_found'] = 'Show the default text when no results are returned for struct value syntax';
+$lang['bottomoutput'] = 'Display data at the bottom of the page';
